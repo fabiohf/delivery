aea35caeb64480de646e12e77048098645a7cf32
Fabio Souza <fabio_hf@hotmail.com>
2015-12-02 19:51:18 -0200

    Listando as categorias
    - Buscando categorias do banco de dados
    - Melhorando pagina de listagem
    - Adicionando paginação

--- app/Http/Controllers/CategoriasController.php
+++ app/Http/Controllers/CategoriasController.php
@@ -3,11 +3,13 @@
 namespace Delivery\Http\Controllers;
 
 use Delivery\Http\Requests;
+use Delivery\Repositories\CategoriaRepository;
 
 class CategoriasController extends Controller
 {
-    public function index()
+    public function index(CategoriaRepository $repository)
     {
-        return view('admin.categorias.index');
+        $categorias = $repository->paginate(5);
+        return view('admin.categorias.index', compact('categorias'));
     }
 }
--- resources/views/admin/categorias/index.blade.php
+++ resources/views/admin/categorias/index.blade.php
@@ -1,1 +1,39 @@
-<h1>Categorias</h1>
+@extends('app')
+
+@section('content')
+
+    <div class="container">
+        <div class="page-header">
+            <h3>Categorias</h3>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading text-right">
+                <div class="btn-group">
+                    <a href="#" class="btn btn-default">Nova Mercadoria</a>
+                </div>
+            </div>
+            <div class="panel-body">
+                <table class="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nome</th>
+                        <th>Ação</th>
+                    </tr>
+                    </thead>
+                    @foreach($categorias as $categoria)
+                        <tr>
+                            <th>{{ $categoria->id }}</th>
+                            <th>{{ $categoria->nome }}</th>
+                            <th></th>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
+            <div class="panel-footer text-center">
+                {!! $categorias->render() !!}
+            </div>
+        </div>
+    </div>
+
+@endsection
